ac1e3f55076403dedd4a686fd30820c5088b2e9d
KeuHarry <57873713+KeuHarry@users.noreply.github.com>
2021-05-07 15:51:22 +0100

    Create pratice.sql

---
+++ pratice.sql
@@ -0,0 +1,18 @@
+-- This is the first query:
+
+SELECT DISTINCT year from population_years;
+
+-- Add your additional queries below:
+--Question 4
+select max(population) from population_years where country = 'Gabon';
+
+--Question 5
+select country from population_years where year = 2005 order by population asc limit 10;
+
+--Question 6
+select distinct country from population_years where population > 100 and year = 2010;
+
+--Question 7
+select count (distinct country) from population_years where country like '%Islands%';
+
+--Question 8
